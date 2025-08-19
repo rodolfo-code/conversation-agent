f1a113c8967e4206da9288e53fb37694bcd8ead6
Rodolfo Oliveira <rodolfo.oliveira@impar.com.br>
2025-08-19 11:34:30 -0300

    adicionando ferramenta de coleta de dados do cliente

--- app/application/agent/conversation_agent/node_functions/how_it_works_node/how_it_works_node.py
+++ app/application/agent/conversation_agent/node_functions/how_it_works_node/how_it_works_node.py
@@ -2,21 +2,31 @@ from typing import Dict, Any
 from app.domain.state.conversation_agent_state import ConversationAgentState
 from app.infrastructure.llm.llm_factory import LLMFactory
 from langchain_core.messages import AIMessage
+import logging
 
+logger = logging.getLogger(__name__)
 
 def how_it_works_node(state: ConversationAgentState) -> Dict[str, Any]:
     """
         Recebe input do supervisor e retorna informaçoes de como funciona o serviço.
     """
 
-    llm_service = LLMFactory.create_llm_service("openai")
-    how_the_clean_works_content = llm_service.clean_works()
+    print("BLABLABLABLABLABLA", state)
 
-    updated_messages = state['messages'] + [AIMessage(content=how_the_clean_works_content)]
+    try:
 
-    return {
-        **state,
-        "messages": updated_messages,
-        "used_nodes": state["used_nodes"] + ["HOW_IT_WORKS_NODE"],
-        "response": how_the_clean_works_content,
-    }
+        llm_service = LLMFactory.create_llm_service("openai")
+        how_the_clean_works_content = llm_service.clean_works()
+
+        updated_messages = state['messages'] + [AIMessage(content=how_the_clean_works_content)]
+
+        return {
+            **state,
+            "messages": updated_messages,
+            "used_nodes": state["used_nodes"] + ["HOW_IT_WORKS_NODE"],
+            "response": how_the_clean_works_content,
+        }
+    
+    except Exception as e:
+        logger.error("Error in how it works node.", extra={"error": e})
+        raise e
--- app/application/agent/conversation_agent/node_functions/how_it_works_node/how_the_clean_works_prompt.py
+++ app/application/agent/conversation_agent/node_functions/how_it_works_node/how_the_clean_works_prompt.py
@@ -14,6 +14,23 @@ HOW_THE_CLEAN_WORKS_TEMPLATE = ChatPromptTemplate.from_template(
     Respoonda com algo entre a formalidade e a informalidade.
     Responda de forma profissional, mas amigável e prestativa. Use parágrafos curtos e listas para facilitar a leitura.
 
+    REFERENCIA/MODELO de RESPOSTA ACEITAVEL:
+        A Doutor Sofá trabalha exclusivamente com produtos homologados pela ANVISA, que possuem ação bactericida, antiácaros, antimicrobiana e fungicida. Isso garante maior proteção ao seu patrimônio e à saúde da sua família.
+        Utilizamos um estabilizador de pH que neutraliza agentes tensoativos, reduzindo a tensão e o desgaste do tecido, o que proporciona mais resistência e maior durabilidade ao estofado após a secagem.
+        Nosso método é a lavagem semi-seca, que permite a remoção de impurezas até 5 cm abaixo da superfície do estofado. Além disso:
+        Realizamos higienização de alta qualidade com extração industrial de todos os resíduos de lavagem;
+        Eliminamos odores de diversas origens, como suor, mofo, vômito ou similares;
+        A secagem é rápida, ocorrendo em 8 a 12 horas em ambiente ventilado, independente do clima;
+        O serviço é feito diretamente em seu endereço, sem sujeira ou incômodos, com duração média de 45 minutos a 1 hora.
+        Observações importantes:
+        Não prometemos a remoção de todas as manchas, pois fatores como o tipo de agente causador, o tempo de contato e as características do tecido podem ocasionar tingimento permanente. Nosso compromisso é sempre preservar a segurança e a durabilidade do seu estofado.
+        Não recomendamos exposição ao sol após a higienização, nem a utilização de panos (com ou sem produtos) sobre o estofado.
+        Quaisquer solicitações de reparo devem ser feitas em até 48 horas após a higienização inicial.
+        Caso seja identificada a necessidade de retorno por resíduos de sujeira que não sejam manchas impregnadas, a empresa fará todos os esforços para agendar uma nova visita dentro do prazo máximo de 48 horas após o contato inicial.
+        Nossa garantia é referente à limpeza e higienização. Seu estofado sairá renovado em diferentes aspectos: na aparência, no toque e na sensação de frescor.
+    
+    Você pode incluir quebras de linha e bullet points para organizar melhor as informações
+
     Pergunta do cliente: {input}
     """
 )
